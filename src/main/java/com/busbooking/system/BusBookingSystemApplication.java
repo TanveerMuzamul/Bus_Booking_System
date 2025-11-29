be077a4d745dfcc5fd16d6bc1726b6b112ca3643
Tanveer Muzamul <a00325116@student.tus.ie>
2025-11-29 13:53:32 +0000

    git add . git commit -m "fix: force SonarQube to recognize logger implementation changes" git push origin main

--- src/main/java/com/busbooking/system/BusBookingSystemApplication.java
+++ src/main/java/com/busbooking/system/BusBookingSystemApplication.java
@@ -1,5 +1,5 @@
 package com.busbooking.system;
-//SonarQube fix: Replaced System.out with logger - commit 5072ec6
+//SONARQUBE FIX: All System.out statements replaced with logger - Force update
 import com.busbooking.system.model.Bus;
 import com.busbooking.system.model.User;
 import com.busbooking.system.repository.BusRepository;
